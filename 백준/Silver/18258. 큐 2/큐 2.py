370230e86b096c8aa5be52510e74239de7946364
t-211 <129669140+t-211@users.noreply.github.com>
2025-12-03 22:10:10 +0900

    [Silver IV] Title: 큐 2, Time: 1588 ms, Memory: 112508 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+
+queue = deque()
+
+for _ in range(N):
+    s = sys.stdin.readline().split()
+    
+    if s[0] == "push":
+        queue.append(int(s[1]))
+    elif s[0] == "pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif s[0] == "size":
+        print(len(queue))
+    elif s[0] == "empty":
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif s[0] == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif s[0] == "back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
+    
