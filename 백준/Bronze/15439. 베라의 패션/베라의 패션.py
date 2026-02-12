da64556bb78f917d0403ee307327f11673936e4a
t-211 <129669140+t-211@users.noreply.github.com>
2026-02-12 16:53:43 +0900

    [Bronze IV] Title: 베라의 패션, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15439. 베라의 패션/베라의 패션.py
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.py
@@ -1,13 +1,3 @@
 N = int(input())
 
-top = [i for i in range(N)]
-pants = [i for i in range(N)]
-
-count = 0
-
-for t in top:
-    for p in pants:
-        if t != p:
-            count += 1
-            
-print(count)
+print(N * (N-1))
